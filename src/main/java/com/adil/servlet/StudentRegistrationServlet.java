ef8a7cc8d480dddd988a0fc9cbbc7f432fcdf90e
mdadilfaiz48 <mdadilfaiz48@gmail.com>
2026-06-01 14:13:23 +0530

    first-comit

---
+++ src/main/java/com/adil/servlet/StudentRegistrationServlet.java
@@ -0,0 +1,112 @@
+package com.adil.servlet;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Vector;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
+
+@WebServlet("/uploadurl")
+@MultipartConfig
+public class StudentRegistrationServlet extends HttpServlet {
+	private static final String INSER_QUERY = "Insert into student_details values(?,?,?,?,?)";
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		// set Response content type
+		response.setContentType("text/html");
+
+		// read special request object
+		try {
+             
+			int id = Integer.parseInt(request.getParameter("sid"));
+			
+			String sname = request.getParameter("sname");
+
+			String address = request.getParameter("saddr");
+
+			Part sresume = request.getPart("resume");
+
+			String resume = sresume.getSubmittedFileName();
+
+			Part sphoto = request.getPart("photo");
+
+			String photo = sphoto.getSubmittedFileName();
+
+			String uploadPath = "C:/adil/";
+
+			// Photo upload
+			InputStream photoInputStream = sphoto.getInputStream();
+
+			Path photoPath = Paths.get(uploadPath, photo);
+
+			Files.copy(photoInputStream, photoPath, java.nio.file.StandardCopyOption.REPLACE_EXISTING);
+
+			// Resume upload
+			InputStream resumeInputStream = sresume.getInputStream();
+
+			Path resumePath = Paths.get(uploadPath, resume);
+
+			Files.copy(resumeInputStream, resumePath, java.nio.file.StandardCopyOption.REPLACE_EXISTING);
+
+			out.println("<h1>File uploaded successfully</h1>");
+			
+			// get history
+		
+			// write jdbc code to save form data and file data in table.
+
+			// Load jdbc driver class.
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// stablish the connection
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "password");
+			// create Prepared statament object
+			  PreparedStatement ps = con.prepareStatement(INSER_QUERY);
+			  // set values in ps
+			   ps.setInt(1, id);
+			   ps.setString(2, sname);
+			   ps.setString(3, address);
+			   ps.setString(4,resume);
+			   ps.setString(5, photo);
+			  
+			   int count = ps.executeUpdate();
+			  
+			   if(count>0) {
+				   
+				   out.println("<h1>Student details insert successfully</h1>");
+			   }else {
+				   
+				   out.println("<h1>Student details not inserted</h1>");
+			   }
+			   //close resources
+			   ps.close();
+			   con.close();
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+			out.println("<h1>Folder not search");
+		}
+
+	}
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+}
---
+++ src/main/java/com/adil/servlet/StudentReportServlet.java
@@ -0,0 +1,116 @@
+package com.adil.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/reporturl")
+public class StudentReportServlet extends HttpServlet {
+	private static final String GET_ALL_STUDENT = "select * from student_details";
+	
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// get PrintWriter
+		PrintWriter out = response.getWriter();
+		// set Response Content type
+		response.setContentType("text/html");
+		// load jdbc driver class
+		 try {
+			  Class.forName("com.mysql.cj.jdbc.Driver");
+		 }catch(ClassNotFoundException cnf) {
+			cnf.printStackTrace();
+		 }
+		//Write jdbc code for get Data from data table
+		try(Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "password");
+				PreparedStatement ps = con.prepareStatement(GET_ALL_STUDENT); ResultSet rs = ps.executeQuery();){ 
+			 // process the result and display the context as html table
+			out.println(
+				    "<table border='1' align='center' bgcolor='cyan'>"
+				);
+
+				out.println(
+				    "<tr>"
+				    + "<th>s_id</th>"
+				    + "<th>sname</th>"
+				    + "<th>saddress</th>"
+				    + "<th>resume</th>"
+				    + "<th>photo</th>"
+				    + "<th>Resume Download</th>"
+				    + "<th>Photo Download</th>"
+				    + "</tr>"
+				);
+
+				while (rs.next()) {
+
+				    out.println("<tr>");
+
+				    out.println(
+				        "<td>" + rs.getInt(1) + "</td>"
+				    );
+
+				    out.println(
+				        "<td>" + rs.getString(2) + "</td>"
+				    );
+
+				    out.println(
+				        "<td>" + rs.getString(3) + "</td>"
+				    );
+
+				    out.println(
+				        "<td>" + rs.getString(4) + "</td>"
+				    );
+
+				    out.println(
+				        "<td>" + rs.getString(5) + "</td>"
+				    );
+
+				    out.println(
+				        "<td>"
+				        + "<a href='downloadurl?resumeId="
+				        + rs.getInt(1)
+				        + "'>Download Resume</a>"
+				        + "</td>"
+				    );
+
+				    out.println(
+				        "<td>"
+				        + "<a href='downloadurl?photoId="
+				        + rs.getInt(1)
+				        + "'>Download Photo</a>"
+				        + "</td>"
+				    );
+
+				    out.println("</tr>");
+				}
+
+				out.println("</table>");
+
+				// Home hyperlink
+				out.println(
+				    "<br><a href='index.html'>Home</a>"
+				);
+			 // close stream
+			 out.close();
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request,response);
+
+	}
+}
